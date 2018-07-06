1d4a5d03f34c2ffc21fe2de589cbd78be264d42f
Jordan Osborn <jordanosborn0@gmail.com>
2018-07-06 23:12:31 +0100

    build handling

--- handlers/BuildHandler.py
+++ handlers/BuildHandler.py
@@ -2,9 +2,12 @@ import tornado.ioloop
 import tornado.web
 
 class BuildHandler(tornado.web.RequestHandler):
-    def initialize(self, **kwargs):
-        self.kwargs = kwargs
+    def initialize(self, docker_env):
+        self.docker_env = docker_env
 
-    def get(self):
-        self.write()
+    def post(self):
+        #if not pull if whitelisted fetch localhost:.../pull/image
+        docker_files = {key: value[0]['body'] for key, value in self.request.files.items()}
+        print(docker_files)
+        self.write('Built')
 
